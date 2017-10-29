180517b9f1eccaeb00754cc30ec7f908877eb68f
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-29 22:55:12 +0000

    fix coverage report to coveralls

--- test/util/install-app-4.ts
+++ test/util/install-app-4.ts
@@ -1,13 +1,10 @@
 import {instantiateApp4} from "./instantiate-generator";
 import * as yo from 'yeoman-test';
 import {get4appPrompts} from "./get4appPrompts";
-import {ConfigKey} from "../../util/ConfigKey";
 
 const app = instantiateApp4();
-const prompts = get4appPrompts();
-prompts[ConfigKey.installDeps] = true;
 
-yo.mockPrompt(app, prompts);
+yo.mockPrompt(app, get4appPrompts());
 
 app.run((e: Error) => {
   if (e) {
